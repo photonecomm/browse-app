0c24be504dc1fcd1f8030e8c7af14a6de661a6c5
photonecomm <36744441+photonecomm@users.noreply.github.com>
2018-06-07 15:15:15 +0530

    Update App.js

--- src/ui/components/app/App.js
+++ src/ui/components/app/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
 	}
 	render() {
 		return (
-			<div className='app-page'>Browse App</div>
+			<div className='app-page'>Browse App V1</div>
 		);
 	}
 };
