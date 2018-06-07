d856ca2c1af55c23733a9bbfaac35b1f57fd5aee
photonecomm <36744441+photonecomm@users.noreply.github.com>
2018-06-07 14:36:15 +0530

    Update ServerEntry.js

--- src/ui/ServerEntry.js
+++ src/ui/ServerEntry.js
@@ -25,7 +25,7 @@ app.get(pageURL, function(req, res) {
 		<!DOCTYPE html>
 		<html lang="en">
 			<head>
-				<title>Browse App</title>
+				<title>Browse App v2</title>
 			</head>
 			<body>
 				${htmlString}
